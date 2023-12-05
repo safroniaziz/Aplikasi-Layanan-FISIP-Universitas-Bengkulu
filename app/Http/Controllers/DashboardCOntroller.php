86d88d4e1cb49d03030eeb4c4ce4a21243fbf924
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-12-05 13:51:36 +0700

    new commit

--- app/Http/Controllers/DashboardCOntroller.php
+++ app/Http/Controllers/DashboardCOntroller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class DashboardCOntroller extends Controller
+class DashboardController extends Controller
 {
     public function index(){
         return view('backend.dashboard.index');
