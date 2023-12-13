04871a5815cc44322deed953a56808c7c4b50f25
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-12-13 16:17:03 +0700

    new commit

--- database/seeders/UserRolePermissionSeeder.php
+++ database/seeders/UserRolePermissionSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserRolePermissionSeeder extends Seeder
@@ -19,6 +20,15 @@ class UserRolePermissionSeeder extends Seeder
         // Buat peran "operator" dan tambahkan ke database
         $roleOperator = Role::create(['name' => 'operator']);
         $roleUser = Role::create(['name' => 'user']);
+
+        $permission = Permission::create(['name'  =>  'dashboard']);
+
+        $permission = Permission::create(['name'  =>  'ruanganPoadcast']);
+        $permission = Permission::create(['name'  =>  'ruanganPoadcast.create']);
+        $permission = Permission::create(['name'  =>  'ruanganPoadcast.store']);
+        $permission = Permission::create(['name'  =>  'ruanganPoadcast.edit']);
+        $permission = Permission::create(['name'  =>  'ruanganPoadcast.update']);
+        $permission = Permission::create(['name'  =>  'ruanganPoadcast.delete']);
 
         $user = User::create([
             'name' => 'Operator',
