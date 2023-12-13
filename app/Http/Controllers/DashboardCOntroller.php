df3135d299abefcfce5a183e70fb8207d26473f5
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-12-13 17:08:06 +0700

    new commit

--- app/Http/Controllers/DashboardCOntroller.php
+++ app/Http/Controllers/DashboardCOntroller.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class DashboardController extends Controller
 {
     public function index(){
+        if (!Gate::allows('dashboard')) {
+            abort(403);
+        }
         return view('backend.dashboard.index');
     }
 }
